9f993079b39f5cbd651e3408cb64111cb5eb1759
Ryan Gibson <ryagibso@gmail.com>
2017-08-10 12:26:45 -0700

    Add files via upload
    Rest of webpage, currently.

---
+++ scripts/Scroll Animation.js
@@ -0,0 +1,9 @@
+﻿src="./scripts/jquery-3.1.1.min.js">
+
+    <script src="jquery.localscroll.js" type="text/javascript"></script>
+    <script src="jquery.scrollTo.js" type="text/javascript"></script> 
+    <!--<script type="text/javascript">
+    $(document).ready(function() {
+        $('#nav').localScroll({duration:800});
+    });
+</script>-->
